581c6095304f32c32496075fa68fc24e18fabb6c
YiminGong <htgzhgym@gmail.com>
2022-03-07 19:54:20 -0600

    add framework. add plans in readme.

---
+++ Assets/Scripts/UI/UIMainMenu.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class UIMainMenu : MenuBase {
+    #region Editor
+
+    [SerializeField]
+    private Button m_newGameButton;
+    [SerializeField]
+    private Button m_quitButton;
+
+    #endregion
+
+    #region Unity Callbacks
+
+    private void Awake() {
+        m_newGameButton.onClick.AddListener(HandleNewGame);
+        m_quitButton.onClick.AddListener(HandleQuit);
+    }
+
+    #endregion
+
+    #region ButtonHandlers
+
+    private void HandleNewGame() {
+        SceneManager.LoadScene("SampleScene"); // change to whichever scene is your next
+        AudioManager.instance.PlayOneShot("menu-click-default");
+    }
+
+    private void HandleQuit() {
+        Application.Quit();
+        AudioManager.instance.PlayOneShot("menu-click-default");
+
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#endif
+    }
+
+    #endregion
+}
---
+++ README.md
@@ -0,0 +1,18 @@
+# MutantTD_Draft1
+
+## Assigned:
+ - Theme: Manipulate probabilities and genetic mutation
+    - idea:
+        1. realize that mutation is a change of DNA sequence.
+        2. realize that not all mutation are useful, but without mutation people also cannot survive. 
+ - Advanced feature: Game tools 
+ - Prototype: Tower Defense
+
+## Plan for draft1:
+    - create 3 basic types of tower. (pure red, green and blue).
+        - have simple DNA sequence. 
+    - create different types of monsters. 
+        - example: circles. RGB(128,0,0) - resistance for red - 128/(2*256) = 25%
+    - for each level completed, player will get MutantPoints. 
+    - use firebase for login, and record player level and MutantPoints.
+    
